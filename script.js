5cf1080f1c7a30e7fad481f7ac93b0a6675d7347
Anurag keshav <anuragkeshav23@gmail.com>
2026-01-04 13:42:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple greeting in console
+console.log("Welcome to my portfolio website");
+
+// Small interaction example
+document.addEventListener("click", function () {
+    console.log("User is interacting with the page");
+});
